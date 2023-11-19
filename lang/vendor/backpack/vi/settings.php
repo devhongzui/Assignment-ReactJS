1850439a8668bde37c7b2f60f6e57eb50cb3f3d7
Tri Nguyen Duc <trind@ecommage.com>
2023-11-19 16:25:19 +0700

    Create Setting Page to Admin

---
+++ lang/vendor/backpack/vi/settings.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Settings Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for Laravel Backpack - Settings
+    |
+    */
+
+    'name' => 'Tên',
+    'value' => 'Giá trị',
+    'description' => 'Mô tả',
+    'setting_singular' => 'Cấu hình',
+    'setting_plural' => 'Các Cấu hình',
+
+];
--- resources/views/vendor/backpack/ui/inc/menu_items.blade.php
+++ resources/views/vendor/backpack/ui/inc/menu_items.blade.php
@@ -1,13 +1,5 @@
 <x-backpack::menu-item :title="__('backpack::base.dashboard')" :link="backpack_url('dashboard')"
                        icon="fa-solid fa-house" />
-<x-backpack::menu-item :title="__('backpack.activity-log::activity_log.activity_logs')"
-                       :link="backpack_url('activity-log')" icon="fa-solid fa-bars-staggered" />
-<x-backpack::menu-item :title="__('backpack::backup.backups')" :link="backpack_url('backup')"
-                       icon="fa-solid fa-hard-drive" />
-<x-backpack::menu-item :title="__('backpack::crud.file_manager')" icon="fa-solid fa-file"
-                       :link="backpack_url('elfinder')" />
-<x-backpack::menu-item :title="__('backpack::logmanager.logs')" :link="backpack_url('log')"
-                       icon="fa-solid fa-terminal" />
 <x-backpack::menu-dropdown :title="__('Add-ons')" icon="fa-solid fa-puzzle-piece">
     <x-backpack::menu-dropdown-header :title="__('Authentication')" />
     <x-backpack::menu-dropdown-item :title="__('backpack::permissionmanager.users')"
@@ -16,4 +8,16 @@
                                     :link="backpack_url('role')" icon="fa-solid fa-users" />
     <x-backpack::menu-dropdown-item :title="__('backpack::permissionmanager.permission_plural')"
                                     :link="backpack_url('permission')" icon="fa-solid fa-key" />
+    <x-backpack::menu-dropdown-header :title="__('File')" />
+    <x-backpack::menu-dropdown-item :title="__('backpack.activity-log::activity_log.activity_logs')"
+                                    :link="backpack_url('activity-log')" icon="fa-solid fa-bars-staggered" />
+    <x-backpack::menu-dropdown-item :title="__('backpack::logmanager.logs')" :link="backpack_url('log')"
+                                    icon="fa-solid fa-terminal" />
+    <x-backpack::menu-dropdown-item :title="__('backpack::backup.backups')" :link="backpack_url('backup')"
+                                    icon="fa-solid fa-hard-drive" />
+    <x-backpack::menu-dropdown-item :title="__('backpack::crud.file_manager')" icon="fa-solid fa-file"
+                                    :link="backpack_url('elfinder')" />
+    <x-backpack::menu-dropdown-header :title="__('Others')" />
+    <x-backpack::menu-dropdown-item :title="__('backpack::settings.setting_plural')" :link="backpack_url('setting')"
+                                    icon="fa-solid fa-gear" />
 </x-backpack::menu-dropdown>
