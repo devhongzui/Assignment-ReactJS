ff099128aeb09518cfa72a6f4ae3fac2ce41a822
Tri Nguyen Duc <trind@ecommage.com>
2023-11-19 15:06:16 +0700

    Create Log Management Page to Admin

---
+++ lang/vendor/backpack/vi/logmanager.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Backpack\LogManager Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'log_manager' => 'Quản lý Nhật ký',
+    'log_manager_description' => 'Xem trước, tải xuống và xóa Nhật ký.',
+    'actions' => 'Hành động',
+    'delete_confirm' => 'Bạn có chắc chắn muốn xóa tệp nhật ký này không?',
+    'back_to_all_logs' => 'Quay lại tất cả nhật ký',
+    'create_a_new_log' => 'Tạo nhật ký mới',
+    'date' => 'Ngày',
+    'delete' => 'Xóa bỏ',
+    'delete_confirmation_title' => 'Xong',
+    'download' => 'Tải xuống',
+    'delete_error_title' => 'Lỗi',
+    'existing_logs' => 'Nhật ký hiện có',
+    'file_name' => 'Tên tập tin',
+    'file_size' => 'Kích thước tập tin',
+    'last_modified' => 'Sửa đổi lần cuối',
+    'logs' => 'Các Nhật ký',
+    'preview' => 'Xem trước',
+    'delete_cancel_title' => 'Không sao đâu',
+    'delete_cancel_message' => 'Tệp nhật ký CHƯA bị xóa.',
+    'delete_error_message' => 'Tệp nhật ký KHÔNG thể xóa.',
+    'delete_confirmation_message' => 'Tệp nhật ký đã bị xóa.',
+    'log_file_doesnt_exist' => 'Tệp nhật ký không tồn tại.',
+
+];
--- resources/views/vendor/backpack/ui/inc/menu_items.blade.php
+++ resources/views/vendor/backpack/ui/inc/menu_items.blade.php
@@ -1,10 +1,10 @@
-{{-- This file is used for menu items by any Backpack v6 theme --}}
 <x-backpack::menu-item :title="__('backpack::base.dashboard')" :link="backpack_url('dashboard')"
                        icon="fa-solid fa-house" />
 <x-backpack::menu-item :title="__('backpack.activity-log::activity_log.activity_logs')"
                        :link="backpack_url('activity-log')" icon="fa-solid fa-bars-staggered" />
 <x-backpack::menu-item :title="__('backpack::backup.backups')" :link="backpack_url('backup')"
                        icon="fa-solid fa-hard-drive" />
-
-<x-backpack::menu-item :title="trans('backpack::crud.file_manager')" icon="fa-solid fa-file"
+<x-backpack::menu-item :title="__('backpack::crud.file_manager')" icon="fa-solid fa-file"
                        :link="backpack_url('elfinder')" />
+<x-backpack::menu-item :title="__('backpack::logmanager.logs')" :link="backpack_url('log')"
+                       icon="fa-solid fa-terminal" />
