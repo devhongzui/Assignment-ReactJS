32f86b11f80ab05c61ab9335addb8f5b1e312e12
Tri Nguyen Duc <trind@ecommage.com>
2023-11-22 11:06:21 +0700

    Change style in Lesson page

--- resources/views/study/lesson/list/item.blade.php
+++ resources/views/study/lesson/list/item.blade.php
@@ -6,14 +6,14 @@
    class="@isset($old) opacity-50 @elseif(isset($recent)) fw-bolder @endisset ">
     <div class="row mb-2">
         <div class="col-3">
-            <img data-src="{{ $src_lesson }}" alt="{{ $item->title }}" class="object-fit-contain img-fluid"
-                 style="max-height: 45px;">
+            <img src="{{ asset('logo.png') }}" data-src="{{ $src_lesson }}" alt="{{ $item->title }}"
+                 class="object-fit-contain" style="width: 100%; height: 45px;">
         </div>
         <div class="col-9 overflow-hidden" data-bs-toggle="tooltip" data-bs-placement="top"
              data-bs-title="{{ $item->title }}"
              style="display: -webkit-box;
-                             -webkit-box-orient: vertical;
-                             -webkit-line-clamp: 1;">
+                    -webkit-box-orient: vertical;
+                    -webkit-line-clamp: 1;">
             {{ $item->title }}
         </div>
     </div>
