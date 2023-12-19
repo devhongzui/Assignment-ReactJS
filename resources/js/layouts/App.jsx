e3cdefd712850ba8a723f132244413f422d16e4d
Tri Nguyen Duc <trind@ecommage.com>
2023-12-19 21:23:43 +0700

    Add Back to top Button

--- resources/js/layouts/App.jsx
+++ resources/js/layouts/App.jsx
@@ -3,20 +3,18 @@ import Footer from "./partials/footer/Footer.jsx";
 import { Outlet } from "react-router-dom";
 import Toast from "./partials/main/Toast.jsx";
 import { Suspense } from "react";
-import { useSelector } from "react-redux";
-import { userData } from "../reduxers/user.jsx";
+import Actions from "./partials/main/Actions.jsx";
 
 export default function App() {
-    const user = useSelector(userData);
-
     return (
         <Suspense fallback={<div>...loading</div>}>
             <Header />
             <main>
                 <Toast />
                 <Outlet />
+                <Actions />
             </main>
-            {user && !user["email_verified_at"] ? null : <Footer />}
+            <Footer />
         </Suspense>
     );
 }
--- resources/js/layouts/partials/footer/Footer.jsx
+++ resources/js/layouts/partials/footer/Footer.jsx
@@ -1,9 +1,13 @@
 import Nav from "./Nav.jsx";
 import Subscribe from "./Subscribe.jsx";
 import Copyright from "./Copyright.jsx";
+import { useSelector } from "react-redux";
+import { userData } from "../../../reduxers/user.jsx";
 
 export default function Footer() {
-    return (
+    const user = useSelector(userData);
+
+    return user && !user["email_verified_at"] ? null : (
         <footer>
             <nav className="navbar bg-body-tertiary py-5">
                 <div className="container">
