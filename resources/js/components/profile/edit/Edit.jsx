d2bbc6c03d42f9e6b5a6d264509a3310a0ebf170
Tri Nguyen Duc <trind@ecommage.com>
2023-12-21 00:52:23 +0700

    Fix logic in Edit Profile

--- resources/js/components/profile/edit/Edit.jsx
+++ resources/js/components/profile/edit/Edit.jsx
@@ -1,7 +1,7 @@
 import { useTranslation } from "react-i18next";
 import { initSite, urlHelper } from "../../../helper.js";
 import { useDispatch, useSelector } from "react-redux";
-import { userData } from "../../../reduxers/user.jsx";
+import { refreshUser, userData } from "../../../reduxers/user.jsx";
 import Form from "../../../templates/Form.jsx";
 import Submit from "../../auth/login/Submit.jsx";
 import axios from "axios";
@@ -13,9 +13,13 @@ import PhoneNumber from "./PhoneNumber.jsx";
 import Address from "./Address.jsx";
 import Birthdate from "./Birthdate.jsx";
 import Gender from "./Gender.jsx";
+import i18next from "i18next";
+import { useNavigate } from "react-router-dom";
 
 export default function Edit() {
     const dispatch = useDispatch();
+
+    const navigate = useNavigate();
 
     const { t } = useTranslation();
 
@@ -30,6 +34,8 @@ export default function Edit() {
 
     function callApi(event) {
         event.preventDefault();
+
+        setValidate({});
 
         const {
             name,
@@ -56,9 +62,22 @@ export default function Edit() {
                 gender: gender.value,
             })
             .then((success) => {
-                dispatch(setToast(success.data));
+                const close_event = () => {
+                    navigate(`/${i18next.language}/user/profile-information`);
 
-                setTimeout(() => (location.href = success.data.redirect), 5000);
+                    dispatch(refreshUser());
+
+                    dispatch(setToast(null));
+                };
+
+                dispatch(
+                    setToast({
+                        message: success.data.message,
+                        close_event: close_event,
+                    }),
+                );
+
+                setTimeout(close_event, 5000);
             })
             .catch((error) => {
                 if (error.response.data.errors)
